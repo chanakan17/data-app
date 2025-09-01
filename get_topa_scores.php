d3d0af44354d4b78fa00e43be7dbddebc17f6791
chanakan17 <toonfane2546@gmail.com>
2025-09-01 17:30:56 +0700

    perfect

--- get_topa_scores.php
+++ get_topa_scores.php
@@ -6,22 +6,31 @@ $data = [];
 $sql = "SELECT gs.user_id, u.username, gs.game_name, gs.game_title, gs.score, gs.play_time_str, gs.created_at
         FROM game_scores gs
         JOIN users u ON gs.user_id = u.id
-        ORDER BY gs.game_name, gs.score DESC, 
+        ORDER BY gs.game_name, gs.game_title, gs.score DESC, 
                  STR_TO_DATE(gs.play_time_str, '%i:%s:%f') ASC,
                  gs.created_at ASC";
 
 $result = $conn->query($sql);
 
+$currentGame = '';
+$currentCategory = '';
+$count = 0;
+
 while ($row = $result->fetch_assoc()) {
     $gameName = $row['game_name'];
+    $category = $row['game_title'];
+
     if (!isset($data[$gameName])) {
         $data[$gameName] = [];
     }
 
-    if (count($data[$gameName]) < 3) {
-        $data[$gameName][] = [
+    if (!isset($data[$gameName][$category])) {
+        $data[$gameName][$category] = [];
+        $count = 0;
+    }
+    if (count($data[$gameName][$category]) < 3) {
+        $data[$gameName][$category][] = [
             "username" => $row['username'],
-            "category" => $row['game_title'],
             "score" => (int)$row['score'],
             "time" => $row['play_time_str']
         ];
